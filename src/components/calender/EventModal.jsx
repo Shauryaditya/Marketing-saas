412e2088c4be4709c836c44867b90ee19d37229c
Soumya Moulik <dev4@jlsmiths.com>
2024-08-22 18:27:15 +0530

    first commit

--- src/components/calender/EventModal.jsx
+++ src/components/calender/EventModal.jsx
@@ -39,14 +39,14 @@ const EventModal = ({ show, onClose, event }) => {
   };
 
   return (
-    <div className="fixed text-xs inset-0 flex items-center justify-center z-50">
+    <div className="fixed text-xs  inset-0 flex items-center justify-center z-50">
       {/* Overlay */}
       <div
         className="absolute inset-0 bg-gray-800 opacity-75"
         onClick={onClose}
       />
       {/* Modal Content */}
-      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md mx-4 relative z-10 overflow-auto max-h-[90vh]">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-full max-w-md mx-4 relative z-10 overflow-auto no-scrollbar max-h-[90vh]">
         <button
           onClick={onClose}
           className="absolute top-4 right-4 text-gray-500 hover:text-gray-700"
@@ -145,7 +145,7 @@ const EventModal = ({ show, onClose, event }) => {
                       <img
                         src={platform.platform_logo}
                         alt={platform.platform_name}
-                        className="w-8 h-8 object-cover rounded-full"
+                        className="w-4 h-4 object-cover rounded-full"
                       />
                     )}
                     <div>
