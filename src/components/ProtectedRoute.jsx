91be215904d0e0029ddbe42861267ee4ad742f42
Soumya Moulik <dev4@jlsmiths.com>
2024-09-16 15:40:52 +0530

    up

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,25 @@
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import jwt_decode from "jsonwebtoken";
 
 const ProtectedRoute = ({ element }) => {
   const { token } = useSelector((state) => state.auth);
 
-  // Check if user is authenticated
-  if (!token) {
+  // Function to check if token is expired
+  const isTokenExpired = (token) => {
+    try {
+      const decoded = jwt_decode.decode(token);
+      if (decoded.exp * 1000 < Date.now()) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return true; // If there's any error, treat the token as expired
+    }
+  };
+
+  // Check if the user has a token and if it's expired
+  if (!token || isTokenExpired(token)) {
     return <Navigate to="/sign-in" />;
   }
 
