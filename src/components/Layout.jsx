002772aaf57d4923d1261a158313273caaacd818
Soumya Moulik <dev4@jlsmiths.com>
2024-11-07 13:02:59 +0530

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,7 +8,9 @@ const Layout = ({ children }) => {
       <div className="h-screen border-right top-0 sticky overflow-y-auto no-scrollbar pr-8">
         <Sidebar />
       </div>
-      <div className="w-full pt-2 border-l ">
+      <div className="w-full pt-16 border-l">
+        {" "}
+        {/* Add pt-16 to offset the fixed navbar */}
         <Navbar />
         <div className="flex-1 p-2">{children}</div>
       </div>
